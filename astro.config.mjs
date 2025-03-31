86daf497e4754c0cb3f9ad58e14c0dbbc6426300
LeandroPata <lmfpata@gmail.com>
2025-03-31 16:08:16 +0100

    Fixed umami ID

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import umami from '@yeskunall/astro-umami';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [umami({ id: '94db1cb1-74f4-4a40-ad6c-962362670409' })],
+	integrations: [umami({ id: '70d6c3a8-dd33-4c44-aeb3-da69ddfcd2b9' })],
 	vite: {
 		resolve: {
 			alias: {
