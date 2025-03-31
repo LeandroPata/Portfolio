44f62ef315c47f9a4f01377fa7a5f2ead7f1781c
LeandroPata <lmfpata@gmail.com>
2025-03-31 16:33:04 +0100

    Removed umami astro integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import umami from '@yeskunall/astro-umami';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [umami({ id: '70d6c3a8-dd33-4c44-aeb3-da69ddfcd2b9' })],
 	vite: {
 		resolve: {
 			alias: {
