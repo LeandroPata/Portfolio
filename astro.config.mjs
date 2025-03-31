e04c29c42451001c48ae8cb98efcb25fc6519487
LeandroPata <lmfpata@gmail.com>
2025-03-31 15:53:00 +0100

    Added umami integration for simple analytics implementation

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+	integrations: [umami({ id: '94db1cb1-74f4-4a40-ad6c-962362670409' })],
 	vite: {
 		resolve: {
 			alias: {
