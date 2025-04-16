7db9252bdd30b15d864251e1e9ff27265723e4f8
LeandroPata <lmfpata@gmail.com>
2025-04-16 17:35:59 +0100

    Added navbar test to prod

---
+++ test/E2ETesting/prod/nav.spec.ts
@@ -0,0 +1,50 @@
+import { test, expect } from '@playwright/test';
+import { ui } from '@/src/i18n/ui';
+
+const baseUrl = 'https://leandropata.pt';
+
+for (const locale in ui) {
+	test(`Load all pages in navbar for the ${locale} locale`, async ({
+		page,
+	}) => {
+		await page.goto(`${baseUrl}/${locale}`);
+
+		await page.waitForSelector('#menu-toggle', {
+			state: 'attached',
+			timeout: 30 * 1000,
+		});
+
+		if (await page.locator('#menu-toggle').isVisible())
+			await page.locator('#menu-toggle').click();
+
+		await page.waitForSelector('#navbar', {
+			state: 'visible',
+			timeout: 30 * 1000,
+		});
+
+		const navbar = page.locator('#navbar');
+		const navbarContents = await navbar.allInnerTexts();
+		const navbarLinks = navbarContents[0].split('\n').reverse();
+
+		for (const link of navbarLinks) {
+			if (!link) continue;
+
+			const linkLocator = page.getByRole('link', { name: link });
+			const linkHref = await linkLocator.getAttribute('href');
+
+			await linkLocator.click();
+
+			await expect(page).toHaveURL(`${baseUrl}${linkHref}`, {
+				timeout: 30 * 1000,
+			});
+
+			await page.waitForSelector('#menu-toggle', {
+				state: 'attached',
+				timeout: 30 * 1000,
+			});
+
+			if (await page.locator('#menu-toggle').isVisible())
+				await page.locator('#menu-toggle').click();
+		}
+	});
+}
