4209ebee22d0b88403eefd47a9778d15bdb70717
LeandroPata <lmfpata@gmail.com>
2026-02-19 18:57:17 +0000

    Preparing to transition to CDN delivered images

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,15 @@
-export const LINKS = {
-	GITHUB: 'https://github.com/LeandroPata',
-	LINKEDIN: 'https://www.linkedin.com/in/leandromfpata',
-	EMAIL: 'lmfpata@gmail.com',
-};
+import { version } from '../../package.json'
+
+export const LINKS = 	{
+		GITHUB: "https://github.com/LeandroPata",
+		LINKEDIN: "https://www.linkedin.com/in/leandromfpata",
+		EMAIL: "lmfpata@gmail.com",
+	}
+
+export const REPO = {
+	NAME: 'Portfolio',
+	USER: 'LeandroPata',
+	VERSION: version,
+}
+
+export const CDN = `https://cdn.jsdelivr.net/gh/${REPO.USER}/${REPO.NAME}@${REPO.VERSION}`
