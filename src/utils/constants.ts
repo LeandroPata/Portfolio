19c4cfaa1808b4e09f6268fffce041a0c95df6ce
LeandroPata <lmfpata@gmail.com>
2026-02-21 17:32:35 +0000

    Changed tag back to 'v' back to version tags, since it is supported by jsDelivr. Fixed deploy github action when creating release

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -9,7 +9,7 @@ export const LINKS = {
 export const REPO = {
 	NAME: 'Portfolio',
 	USER: 'LeandroPata',
-	VERSION: version,
+	VERSION: `v${version}`,
 };
 
 export const CDN = `https://cdn.jsdelivr.net/gh/${REPO.USER}/${REPO.NAME}@${REPO.VERSION}`;
