7621041955010a73c61a066369486abf9b81f308
LeandroPata <lmfpata@gmail.com>
2026-04-17 15:49:52 +0100

    Update content config due to Astro 6 changes

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,6 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection } from 'astro:content';
 import { glob } from 'astro/loaders';
+import { z } from 'astro/zod';
 import { languages } from '@/src/i18n/ui';
 
 const projectsSchema = z.object({
