c67466862670e33c87a86459d01ea102760e354e
LeandroPata <lmfpata@gmail.com>
2026-04-17 15:51:07 +0100

    Wrote additional tests, due to bugs found

--- test/E2ETesting/themeToggle.spec.ts
+++ test/E2ETesting/themeToggle.spec.ts
@@ -1,33 +1,79 @@
 import { expect, test } from '@/test/E2ETesting/fixtures';
 
-test('Theme toggle switches between light and dark mode', async ({ page }) => {
-	await page.goto('/', { waitUntil: 'domcontentloaded' });
+test.describe('Theme toggle switches between light and dark mode', () => {
+	test('Theme toggles correctly', async ({ page }) => {
+		await page.goto('/', { waitUntil: 'domcontentloaded' });
 
-	await page.waitForSelector('#menu-toggle', {
-		state: 'attached',
+		await page.waitForSelector('#menu-toggle', {
+			state: 'attached',
+		});
+
+		if (await page.locator('#menu-toggle').isVisible())
+			await page.locator('#menu-toggle').click();
+
+		const html = page.locator('html');
+		const button = page.locator('theme-toggle button');
+
+		const initialHasDark = await html.evaluate((el) =>
+			el.classList.contains('theme-dark'),
+		);
+
+		await button.click();
+		const afterClickHasDark = await html.evaluate((el) =>
+			el.classList.contains('theme-dark'),
+		);
+
+		expect(afterClickHasDark).not.toBe(initialHasDark);
+
+		await button.click();
+		const endHasDark = await html.evaluate((el) =>
+			el.classList.contains('theme-dark'),
+		);
+
+		expect(endHasDark).toBe(initialHasDark);
 	});
+	test('Theme persists when navigating', async ({ page }) => {
+		await page.goto('/', { waitUntil: 'domcontentloaded' });
 
-	if (await page.locator('#menu-toggle').isVisible())
-		await page.locator('#menu-toggle').click();
+		await page.waitForSelector('#menu-toggle', {
+			state: 'attached',
+		});
 
-	const html = page.locator('html');
-	const button = page.locator('theme-toggle button');
+		if (await page.locator('#menu-toggle').isVisible())
+			await page.locator('#menu-toggle').click();
 
-	const initialHasDark = await html.evaluate((el) =>
-		el.classList.contains('theme-dark'),
-	);
+		const html = page.locator('html');
+		const button = page.locator('theme-toggle button');
 
-	await button.click();
-	const afterClickHasDark = await html.evaluate((el) =>
-		el.classList.contains('theme-dark'),
-	);
+		const initialHasDark = await html.evaluate((el) =>
+			el.classList.contains('theme-dark'),
+		);
+		console.log(initialHasDark);
 
-	expect(afterClickHasDark).not.toBe(initialHasDark);
+		await button.click();
 
-	await button.click();
-	const endHasDark = await html.evaluate((el) =>
-		el.classList.contains('theme-dark'),
-	);
+		const afterClickHasDark = await html.evaluate((el) =>
+			el.classList.contains('theme-dark'),
+		);
+		console.log(afterClickHasDark);
 
-	expect(endHasDark).toBe(initialHasDark);
+		expect(afterClickHasDark).not.toBe(initialHasDark);
+
+		await page.waitForSelector('#navbar', {
+			state: 'visible',
+		});
+
+		const linkLocator = page.getByRole('link', { name: 'Projects' });
+		const linkHref = await linkLocator.getAttribute('href');
+
+		await linkLocator.click();
+		await page.waitForURL(linkHref, { waitUntil: 'domcontentloaded' });
+
+		const afterNavHasDark = await html.evaluate((el) =>
+			el.classList.contains('theme-dark'),
+		);
+		console.log(afterNavHasDark);
+
+		expect(afterNavHasDark).toBe(afterClickHasDark);
+	});
 });
