f068be6cf0f388a1c10747ceb2394e9e111101b0
LeandroPata <lmfpata@gmail.com>
2026-04-20 19:19:35 +0100

    Improved testing for CDN links

--- test/E2ETesting/imageLoading.spec.ts
+++ test/E2ETesting/imageLoading.spec.ts
@@ -7,6 +7,7 @@ const routes = getRoutes(path.resolve('dist'));
 test.describe('CDN links load correctly', () => {
 	for (const route of routes) {
 		test(`CDN links in: ${route}`, async ({ page }) => {
+			const cdnRequests: string[] = [];
 			const failedRequests: string[] = [];
 
 			// Continue all CDN requests
@@ -18,16 +19,35 @@ test.describe('CDN links load correctly', () => {
 			page.on('response', (response) => {
 				if (
 					response.request().resourceType() === 'image' &&
-					response.url().includes('cdn.jsdelivr.net') &&
-					!response.ok()
+					response.url().includes('cdn.jsdelivr.net')
 				) {
-					failedRequests.push(`${response.url()} — ${response.status()}`);
+					cdnRequests.push(`${response.url()} — ${response.status()}`);
+					if (!response.ok())
+						failedRequests.push(`${response.url()} — ${response.status()}`);
 				}
 			});
 
-			await page.goto(route, { waitUntil: 'domcontentloaded' });
+			await page.goto(route, { waitUntil: 'load' });
+
+			const cdnImages = await page
+				.locator('img[src*="cdn.jsdelivr.net"]')
+				.evaluateAll((imgs) =>
+					(imgs as HTMLImageElement[]).map((img) => img.src),
+				);
+
+			console.log(cdnImages);
+			test.skip(!cdnImages.length, 'No CDN links found');
+
+			console.log(cdnRequests);
+			console.log(failedRequests);
+
+			await page
+				.locator('img[src*="cdn.jsdelivr.net"]')
+				.last()
+				.waitFor({ state: 'visible' });
 
 			expect(failedRequests).toEqual([]);
+			expect(cdnImages.length).toBe(cdnRequests.length);
 		});
 	}
 });
