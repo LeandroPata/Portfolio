e982af3e61a7f7e4d9251d52ad0af2a5d85ddaee
LeandroPata <lmfpata@gmail.com>
2026-04-23 16:58:28 +0100

    Improved imageLoading tests

--- test/E2ETesting/imageLoading.spec.ts
+++ test/E2ETesting/imageLoading.spec.ts
@@ -1,4 +1,5 @@
 import path from 'node:path';
+import { countCDNImages } from '@/src/utils/cdnUtils';
 import getRoutes from '@/src/utils/getRoutes';
 import { expect, test } from '@/test/E2ETesting/fixtures';
 
@@ -7,8 +8,11 @@ const routes = getRoutes(path.resolve('dist'));
 test.describe('CDN links load correctly', () => {
 	for (const route of routes) {
 		test(`CDN links in: ${route}`, async ({ page }) => {
+			test.skip(!countCDNImages(route), 'No CDN links found');
+
 			const cdnRequests: string[] = [];
 			const failedRequests: string[] = [];
+			const countCDN = countCDNImages(route);
 
 			// Continue all CDN requests
 			await page.route('https://cdn.jsdelivr.net/**', (route) =>
@@ -21,7 +25,8 @@ test.describe('CDN links load correctly', () => {
 					response.request().resourceType() === 'image' &&
 					response.url().includes('cdn.jsdelivr.net')
 				) {
-					cdnRequests.push(`${response.url()} — ${response.status()}`);
+					if (!cdnRequests.some((r) => r.startsWith(response.url())))
+						cdnRequests.push(`${response.url()} — ${response.status()}`);
 					if (!response.ok())
 						failedRequests.push(`${response.url()} — ${response.status()}`);
 				}
@@ -29,25 +34,16 @@ test.describe('CDN links load correctly', () => {
 
 			await page.goto(route, { waitUntil: 'load' });
 
-			const cdnImages = await page
-				.locator('img[src*="cdn.jsdelivr.net"]')
-				.evaluateAll((imgs) =>
-					(imgs as HTMLImageElement[]).map((img) => img.src),
-				);
-
-			console.log(cdnImages);
-			test.skip(!cdnImages.length, 'No CDN links found');
-
-			console.log(cdnRequests);
-			console.log(failedRequests);
-
 			await page
 				.locator('img[src*="cdn.jsdelivr.net"]')
 				.last()
 				.waitFor({ state: 'visible' });
 
+			//console.log(cdnRequests);
+			//console.log(`${cdnRequests.length} : ${countCDN}`);
+
 			expect(failedRequests).toEqual([]);
-			expect(cdnImages.length).toBe(cdnRequests.length);
+			expect(cdnRequests.length).toBe(countCDN);
 		});
 	}
 });
